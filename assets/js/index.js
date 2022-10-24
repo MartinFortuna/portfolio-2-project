d80673cbad225367621f115b4d546bc008fcd841
MartinFortuna <martinperricault@gmail.com>
2022-10-24 23:10:10 +0000

    Add global variables and event listeners

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,33 @@
+// Global Variables
+
+const xApiKey = "63544228626b9c747864ae40";
+const maxPointsInGame = 10;
+
+// Add Event listeners
+
+function addEventListener() {
+    document.querySelector(".spock").addEventListener("click", runMatch);
+    document.querySelector(".rock").addEventListener("click", runMatch);
+    document.querySelector(".scissors").addEventListener("click", runMatch);
+    document.querySelector(".lizard").addEventListener("click", runMatch);
+    document.querySelector(".paper").addEventListener("click", runMatch);
+    document.querySelector(".form-user-name").addEventListener("submit", userRegister);
+    document.querySelector(".btn-leaderboard").addEventListener("click", renderLearderboard);
+
+    window.onload = () => {
+        toggleLoad();
+        document.querySelector('.username-modal').style.display = "block";
+    }
+}
+
+function toggleLoad() {
+    if (document.querySelector(".preloader").style.display == "none") {
+        document.querySelector(".preloader").style.display = "block"
+    } else {
+        document.querySelector(".preloader").style.display = "none";
+    }
+}
+
 // Leaderboard modal
 const leadModal = document.getElementById("leaderboard-modal");
 const leadBtn = document.getElementById("btn-leaderboard");
