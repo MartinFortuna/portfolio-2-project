4ac7f44164bb4ff343d85ea4d533d4fc8244e9f0
MartinFortuna <martinperricault@gmail.com>
2022-10-25 00:07:38 +0000

    Re-add onclick deleted by mistake

--- assets/js/index.js
+++ assets/js/index.js
@@ -407,6 +407,26 @@ async function rankLeaderboard(data) {
 
 addEventListener();
 
+// Leaderboard modal
+const leadModal = document.getElementById("leaderboard-modal");
+const leadBtn = document.getElementById("btn-leaderboard");
+var span = document.getElementsByClassName("leaderboard-close")[0];
+
+leadBtn.onclick = function () {
+    leadModal.style.display = "block";
+}
+
+span.onclick = function () {
+    leadModal.style.display = "none";
+}
+
+window.onclick = function (event) {
+    if (event.target == leadModal) {
+        leadModal.style.display = "none";
+    }
+}
+
+// Game Rules modal
 const rulesModal = document.getElementById("rules-modal");
 const rulesBtn = document.getElementById("btn-rules");
 var span = document.getElementsByClassName("rules-close")[0];
